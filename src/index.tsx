c08aa5c7814d98335aed80a3b5566b07a06e45e7
LAPTOP-N75N2CF0\86136 <1324071204@qq.com>
2024-01-17 16:09:23 +0800

    ci: 测试2

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-
+// 测试
 root.render(
   <React.StrictMode>
     <App />
